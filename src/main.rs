a9c9c8bb6d00e5bdf634163ed3e0098a3e01d64d
Edvard Thörnros <edvard.thornros@gmail.com>
2021-05-08 00:01:05 +0200

    Fix the bug

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         file: Some(Path::new("game.sy").to_path_buf()),
         is_binary: false,
         compile_target: None,
-        verbosity: 2,
+        verbosity: 0,
         help: false,
     };
 
