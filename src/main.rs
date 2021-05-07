25d7bb372a4ad87935012a11f8bd86b2367b9b45
Gustav Sörnäs <gustav@sornas.net>
2021-05-07 19:39:27 +0200

    run game from src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args = sylt::Args {
+        file: Some(Path::new("game.sy").to_path_buf()),
+        is_binary: false,
+        compile_target: None,
+        verbosity: 0,
+        help: false,
+    };
+
+    sylt::run_file(&args, sylt::lib_bindings()).unwrap();
 }
